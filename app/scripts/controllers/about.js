88c3ae46f2c10f5c5d24e1f9a637e389c22a092f
Calvin De Lima <calvindelima@Calvins-MacBook-Pro.local>
2014-10-13 19:46:39 -0400

    Initial project commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name densitiesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the densitiesApp
+ */
+angular.module('densitiesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name densitiesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the densitiesApp
+ */
+angular.module('densitiesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
