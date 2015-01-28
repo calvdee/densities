414b6bbf75285dd41d9420904b76aa57154e4e2b
Calvin De Lima <calvindelima@Calvins-MacBook-Pro.local>
2015-01-27 19:45:24 -0500

    Renamed barchart directive to density-chart
     Committer: Calvin De Lima <calvindelima@Calvins-MacBook-Pro.local>

---
+++ app/scripts/directives/density-chart.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name densitiesApp.directive:barChart
+ * @description
+ * # barChart
+ */
+angular.module('densitiesApp')
+  .directive('barChart', function (d3) {
+    var chart = d3.custom.density();
+	return {
+        restrict: 'E',
+        replace: true,
+        template: '<div class="chart"></div>',
+        scope:{
+            data: '=data',
+            hovered: '&hovered',
+            opts: '=opts'
+        },
+        link: function(scope, element, attrs) {
+
+            var chartOpts = scope.opts;
+
+            var chartEl = d3.select(element[0]);
+            // chart.on('customHover', function(d, i){
+            //     scope.hovered({args:d});
+            // });
+
+            scope.$watch('data', function (newVal, oldVal) {
+                chartEl.datum(newVal).call(chart, chartOpts);
+            });
+
+            // scope.$watch('height', function(d, i){
+            //     chartEl.call(chart.height(scope.height));
+            // })
+        }
+    };
+  });
